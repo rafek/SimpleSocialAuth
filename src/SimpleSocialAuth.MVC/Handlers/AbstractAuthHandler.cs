df163428a144cf97f60114098621a2784cc2ea01
Rafal Legiedz <rafal.legiedz@gmail.com>
2012-02-07 05:12:09 +0100

    FacebookConsumer and Handler. Added dependencies.

---
+++ src/SimpleSocialAuth.MVC/Handlers/AbstractAuthHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Web;
+
+namespace SimpleSocialAuth.MVC.Handlers
+{
+  public abstract class AbstractAuthHandler
+  {
+    public abstract string PrepareAuthRequest(HttpRequestBase request, string redirectPath, IDictionary<string, string> parameters = null);
+    public abstract BasicUserData ProcessAuthRequest(HttpRequestBase request);
+  }
+}
