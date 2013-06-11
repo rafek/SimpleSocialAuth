569135ca8a3639722be2fb90cc63e69635299990
Ken Smith <smithkl42@gmail.com>
2013-06-11 13:53:29 -0700

    Add error handling to SimpleAuthController.cs.pp

--- nuget/content.common/SimpleAuthController.cs.pp
+++ nuget/content.common/SimpleAuthController.cs.pp
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System;
+using System.Diagnostics;
 using System.Web.Mvc;
 using System.Web.Security;
 using SimpleSocialAuth.Core;
@@ -6,6 +7,11 @@ using SimpleSocialAuth.Core.Handlers;
 
 namespace $rootnamespace$.Controllers
 {
+	/// <summary>
+	/// Simple scaffolding class for handling OAuth-based authentication.
+	/// Many scenarios will require that this controller be extended or logic from this
+	/// controller moved into other classes.
+	/// </summary>
 	public class SimpleAuthController : Controller
 	{
 		public ActionResult LogIn()
@@ -14,46 +20,60 @@ namespace $rootnamespace$.Controllers
 			{
 				return RedirectToAction("Index", "Home");
 			}
-
 			Session["ReturnUrl"] = Request.QueryString["returnUrl"];
-
 			return View();
 		}
 
-		// TODO: HI Errors handling
 		[HttpPost]
 		public ActionResult Authenticate(string authType)
 		{
-			var authHandler = AuthHandlerFactory.Create(authType);
-			var authContext = new PrepareAuthenticationContext(
-				CurrentContextSession.Instance,
-				Request.Url,
-				Url.Action("DoAuth", new { authType }));
-			string redirectUrl = authHandler.PrepareAuthRequest(authContext);
-			return Redirect(redirectUrl);
+			try
+			{
+				var authHandler = AuthHandlerFactory.Create(authType);
+				var authContext = new PrepareAuthenticationContext(
+					CurrentContextSession.Instance,
+					Request.Url,
+					Url.Action("DoAuth", new { authType }));
+				string redirectUrl = authHandler.PrepareAuthRequest(authContext);
+				return Redirect(redirectUrl);
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine("Error calling OAuth provider: " + ex);
+				TempData["authError"] = ex.Message;
+				return RedirectToAction("LogIn");
+			}
 		}
 
 		public ActionResult DoAuth(string authType)
 		{
-			var authHandler = AuthHandlerFactory.Create(authType);
-			var authContext = new ProcessAuthenticationContext(CurrentContextSession.Instance, Request.Url);
-			var userData = authHandler.ProcessAuthRequest(authContext);
-
-			if (userData == null)
+			try
 			{
-				TempData["authError"] = "Authentication has failed.";
+				var authHandler = AuthHandlerFactory.Create(authType);
+				var authContext = new ProcessAuthenticationContext(CurrentContextSession.Instance, Request.Url);
+				var userData = authHandler.ProcessAuthRequest(authContext);
 
+				if (userData == null)
+				{
+					TempData["authError"] = "Authentication has failed.";
+
+					return RedirectToAction("LogIn");
+				}
+
+				// NOTE: this is just simple usage of setting AuthCookie
+				// TODO: You may also want to check to see if this user exists in your database, create an entry if they don't, etc.
+				FormsAuthentication.SetAuthCookie(userData.UserName, true);
+
+				return Session["ReturnUrl"] != null
+					       ? (ActionResult) Redirect((string) Session["ReturnUrl"])
+					       : RedirectToAction("Index", "Home");
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine("Error responding to OAuth request: " + ex);
+				TempData["authError"] = ex.Message;
 				return RedirectToAction("LogIn");
 			}
-
-			// TODO: Here you can check if such user exists in your database, etc.
-
-			// NOTE: this is just simple usage of setting AuthCookie
-			FormsAuthentication.SetAuthCookie(userData.UserName, true);
-
-			return Session["ReturnUrl"] != null
-			  ? (ActionResult)Redirect((string)Session["ReturnUrl"])
-			  : RedirectToAction("Index", "Home");
 		}
 	}
 }
