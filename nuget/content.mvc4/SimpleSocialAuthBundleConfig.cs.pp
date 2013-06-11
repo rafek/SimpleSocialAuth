6418eb1fc67dfb866c1fc671911e9f8e8fa6ae1e
Ken Smith <smithkl42@gmail.com>
2013-06-10 17:37:28 -0700

    Rework script/stylesheet references

---
+++ nuget/content.mvc4/SimpleSocialAuthBundleConfig.cs.pp
@@ -0,0 +1,16 @@
+﻿using System.Web.Optimization;
+
+namespace namespace $rootnamespace$.App_Start
+{
+	public class SimpleSocialAuthBundleConfig
+	{
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.Add(new ScriptBundle("~/bundles/SimpleSocialAuth")
+				.Include("~/Scripts/SimpleSocialAuth.js"));
+
+			bundles.Add(new StyleBundle("~/content/SimpleSocialAuth").Include(
+				"~/Content/SimpleSocialAuth.js"));
+		}
+	}
+}
